b6587f0ef54ff729276747014084bd9ce3cc90da
Siarhei Kren <strannik19990@gmaiol.com>
2022-08-14 22:34:25 +0300

    correct mistakes

--- Citrus/client/src/app/services/auth-guard.service.ts
+++ Citrus/client/src/app/services/auth-guard.service.ts
@@ -5,8 +5,8 @@ import {
 	Router,
 	RouterStateSnapshot,
 } from '@angular/router';
-import { HttpService } from '@services/http.service';
 import { Observable } from 'rxjs';
+import { AuthHttpService } from './auth-http.service';
 
 @Injectable({
 	providedIn: 'root',
@@ -16,12 +16,12 @@ export class AuthGuardService implements CanActivate {
 		next: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot,
 	): Observable<boolean> | boolean {
-		if (this.http.getToken() !== '') {
+		if (this.authHttp.getToken() !== '') {
 			return true;
 		}
 		this.router.navigate(['/']);
 		return false;
 	}
 
-	constructor(private http: HttpService, private router: Router) {}
+	constructor(private authHttp: AuthHttpService, private router: Router) {}
 }
