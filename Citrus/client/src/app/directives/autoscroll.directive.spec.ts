9e5e17d6cdf5ab1c4cc2a60c8f7e3e275129c57d
Sergey Kren <sergey.kren@intexsoft.by>
2023-01-23 18:53:45 +0300

    add tests for add-active-class.directive.ts

---
+++ Citrus/client/src/app/directives/autoscroll.directive.spec.ts
@@ -0,0 +1,58 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { AutoscrollDirective } from '@directives/autoscroll.directive';
+
+@Component({
+	template: ` <li appAutoscroll id="0">MockLi1</li>`,
+})
+export class TestComponent {}
+
+describe('AutoscrollDirective', () => {
+	let fixture: ComponentFixture<TestComponent>;
+	let component: TestComponent;
+	let li: DebugElement;
+	let des: DebugElement;
+	let dir: AutoscrollDirective;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [RouterTestingModule],
+			declarations: [TestComponent, AutoscrollDirective],
+		}).compileComponents();
+	});
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(TestComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+		li = fixture.debugElement.query(By.css('li'));
+		des = fixture.debugElement.query(By.directive(AutoscrollDirective));
+		dir = des.injector.get(AutoscrollDirective) as AutoscrollDirective;
+	});
+
+	afterEach(() => {
+		fixture.destroy();
+	});
+
+	it('component should created', () => {
+		expect(component).toBeDefined();
+	});
+
+	describe('ngAfterViewInit', () => {
+		it('scrollIntoView should not be called', () => {
+			spyOn(li.nativeElement, 'scrollIntoView').and.callThrough();
+			dir.selectedElement = 1;
+			dir.ngAfterViewInit();
+			expect(li.nativeElement.scrollIntoView).not.toHaveBeenCalled();
+		});
+
+		it('scrollIntoView should be called', () => {
+			spyOn(li.nativeElement, 'scrollIntoView').and.callThrough();
+			dir.selectedElement = 0;
+			dir.ngAfterViewInit();
+			expect(li.nativeElement.scrollIntoView).toHaveBeenCalled();
+		});
+	});
+});
